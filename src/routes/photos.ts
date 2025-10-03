bd49499cf17a46f10848618a74bdd20ffd8217c8
Meet Parmar <meetparmar0105@gmail.com>
2025-10-03 19:02:49 +0530

    Complete cleanup: Successfully removed all TypeScript files and directories

--- src/routes/photos.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getPhotoById, getPhotoGallery } from '../controllers/photoController';
-
-const router = Router();
-
-router.get('/image/:id', getPhotoById);
-router.get('/gallery/:id', getPhotoGallery);
-
-export default router;
