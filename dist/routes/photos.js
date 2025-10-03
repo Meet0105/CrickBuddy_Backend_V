cecaee17bc3ffc40197dcf316ef14cb09cf3ef5a
Meet Parmar <meetparmar0105@gmail.com>
2025-10-03 18:41:32 +0530

    Clean up: Remove all unnecessary test files, debug scripts, TypeScript files and dependencies

--- dist/routes/photos.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const photoController_1 = require("../controllers/photoController");
-const router = (0, express_1.Router)();
-router.get('/image/:id', photoController_1.getPhotoById);
-router.get('/gallery/:id', photoController_1.getPhotoGallery);
-exports.default = router;
